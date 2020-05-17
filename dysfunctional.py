d79d4fa3fd7089e30cfc97849858c6c125c239e5
NSQY <34185288+NSQY@users.noreply.github.com>
2020-05-17 06:45:02 +0000

    Update docs, allow for custom KNL strengths, clearly define KNL, proper vs.core import

--- dysfunctional.py
+++ dysfunctional.py
@@ -1,8 +1,16 @@
 import vapoursynth as vs
-from vapoursynth import core
+core = vs.core
 
-def STGrain(clip, **kwargs) -> vs.VideoNode:
+def STGrain(clip, knl=1, **kwargs) -> vs.VideoNode:
     """
+    This can be either imported into the script or defined manually,
+    if you would like to make changes (such as using a different denoiser) I suggest the latter
+    I may add the ability to define a custom denoiser later.
+    KNLMeansCL documentation can be found here: https://github.com/Khanattila/KNLMeansCL/wiki/Filter-description
+    STPresso: https://github.com/HomeOfVapourSynthEvolution/havsfunc/blob/master/havsfunc.py#L3535
+    STPresso args can be parsed directly into this wrapper, i.e;
+    tst = dsf.STGrain(src, knl = 2, limit = 6, bias = 70, tbias = 55, tthr = 32)
+    -
     STPresso is being fed a GRAY16 clip, don't expect anything on [1,2].
     TODO: Handle Y/Cb/Cr
     """
@@ -14,7 +22,7 @@ def STGrain(clip, **kwargs) -> vs.VideoNode:
 
     b16 = Depth(clip, 16)
     lma = core.std.ShufflePlanes(b16, 0, vs.GRAY)
-    dns = core.knlm.KNLMeansCL(lma, h = 2)
+    dns = core.knlm.KNLMeansCL(lma, h=knl, d=2, device_type='gpu', device_id=0, channels='Y')
     dif = core.std.MakeDiff(lma, dns)
     rgn = STPresso(dif, **kwargs)
     mrg = core.std.MergeDiff(dns, rgn)
