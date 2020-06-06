f9facc6ee5774f652674132a9f1d6ba75486c863
NSQY <34185288+NSQY@users.noreply.github.com>
2020-06-06 17:01:20 +0000

    update
    vsutil, prefilter options, filter strength, show_diff

--- dysfunctional.py
+++ dysfunctional.py
@@ -1,30 +1,49 @@
 import vapoursynth as vs
 core = vs.core
 
-def STGrain(clip, knl=1, **kwargs) -> vs.VideoNode:
+def RGBtoYUV(clip):
+    if clip.format.color_family == vs.RGB:
+        return core.resize.Point(clip, format=vs.YUV444P16, matrix_s='709', dither_type='error_diffusion')
+    else:
+        return clip
+
+
+def STGraino(clip, pre=None, str=1, show_diff=False, **kwargs) -> vs.VideoNode:
     """
-    This can be either imported into the script or defined manually,
-    if you would like to make changes (such as using a different denoiser) I suggest the latter
-    I may add the ability to define a custom denoiser later.
     KNLMeansCL documentation can be found here: https://github.com/Khanattila/KNLMeansCL/wiki/Filter-description
     STPresso: https://github.com/HomeOfVapourSynthEvolution/havsfunc/blob/master/havsfunc.py#L3535
     STPresso args can be parsed directly into this wrapper, i.e;
-    tst = dsf.STGrain(src, knl = 2, limit = 6, bias = 70, tbias = 55, tthr = 32)
+    tst = dsf.STGrain(src, pre = 'knl', str = 2, limit = 6, bias = 70, tbias = 55, tthr = 32)
     -
     STPresso is being fed a GRAY16 clip, don't expect anything on [1,2].
-    TODO: Handle Y/Cb/Cr
+    This is getting very dumb very fast
+    TODO: Handle Y/Cb/Cr, core.std.Convolution
     """
-    from fvsfunc import Depth
+    from vsutil import depth as Depth
     from havsfunc import STPresso
+    from cooldegrain import CoolDegrain
 
-    if clip.format.color_family == vs.RGB:
-        clip = core.resize.Point(clip, format=vs.YUV444P16, matrix_s='709')
-
-    b16 = Depth(clip, 16)
+    rgb = RGBtoYUV(clip)
+    b16 = Depth(rgb, 16)
     lma = core.std.ShufflePlanes(b16, 0, vs.GRAY)
-    dns = core.knlm.KNLMeansCL(lma, h=knl, d=2, device_type='gpu', device_id=0, channels='Y')
+
+    if pre is None or 'knl':
+        denoise = core.knlm.KNLMeansCL(lma, h=str, d=2, device_type='gpu', device_id=0, channels='Y')
+    elif pre == 'cool':
+        denoise = CoolDegrain(lma, thsad=str)
+    elif pre == 'rgsf' or 'rgvs':
+        try:
+            denoise = core.rgsf.RemoveGrain(lma, mode=str)
+        except AttributeError:
+            denoise = core.rgvs.RemoveGrain(lma, mode=str)
+        
+    dns = denoise
     dif = core.std.MakeDiff(lma, dns)
     rgn = STPresso(dif, **kwargs)
     mrg = core.std.MergeDiff(dns, rgn)
     shf = core.std.ShufflePlanes([mrg, b16], [0,1,2], vs.YUV)
-    return Depth(shf, clip.format.bits_per_sample)
+
+    if show_diff is True:
+        return dif
+    else: 
+        return Depth(shf, clip.format.bits_per_sample)
