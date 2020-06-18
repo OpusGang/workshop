c744f6af98804ca18d92e09154012f6d6e18ef9a
NSQY <34185288+NSQY@users.noreply.github.com>
2020-06-18 10:41:42 +0000

    big update
    planar support

--- dysfunctional.py
+++ dysfunctional.py
@@ -1,48 +1,52 @@
 import vapoursynth as vs
 core = vs.core
 
-def STGraino(clip, pre=None, str=1, show_diff=False, **kwargs) -> vs.VideoNode:
+def STGraino(clip, planes: [0,1,2],
+            pre = [1,2,2], 
+            str: float = [1,4,4], 
+            show_diff = False,
+            **kwargs) -> vs.VideoNode:
     """
     KNLMeansCL documentation can be found here: https://github.com/Khanattila/KNLMeansCL/wiki/Filter-description
     STPresso: https://github.com/HomeOfVapourSynthEvolution/havsfunc/blob/master/havsfunc.py#L3535
     STPresso args can be parsed directly into this wrapper, i.e;
-    tst = dsf.STGraino(src, pre = 'knl', str = 2, limit = 6, bias = 70, tbias = 55, tthr = 32)
+    grn = dsf.STGraino(b16, planes=[0,1,2], pre=[1,2,2], str = [1.5,4,4], limit = 4, bias = 65, tbias = 50, tthr = 32, show_diff=True)
     -
-    STPresso is being fed a GRAY16 clip, don't expect anything on [1,2].
-    This is getting very dumb very fast
-    TODO: Handle Y/Cb/Cr
+    TODO: parse knlm args, per-plane show_diff
     """
-    from vsutil import depth as Depth
+    from vsutil import split, join, depth as Depth
     from havsfunc import STPresso
+    from rgvs import RemoveGrain
 
     rgb = RGBtoYUV(clip)
     b16 = Depth(rgb, 16)
-    lma = core.std.ShufflePlanes(b16, 0, vs.GRAY)
+    p = split(b16)
 
-    if pre == 'knl':
-        denoise = core.knlm.KNLMeansCL(lma, h=str, d=2, device_type='gpu', device_id=0, channels='Y')
-    elif pre == 'cdg':
-        from cooldegrain import CoolDegrain
-        denoise = CoolDegrain(lma, tr=1, thsad=str)
-    elif pre == 'rgsf' or 'rgvs':
-        from rgvs import RemoveGrain
-        try:
-            denoise = RemoveGrain(lma, mode=str)
-        except AttributeError:
-            denoise = core.rgvs.RemoveGrain(lma, mode=str)
-    else:
-        raise TypeError("STGraino: Invalid pre-filter!")
+    if 0 in planes:
+        if pre[0] == 1:
+            p[0] = p[0].knlm.KNLMeansCL(h=str[0])
+        elif pre[0] == 2:
+            p[0] = RemoveGrain(p[0], mode=str[0])
+    if 1 in planes:
+        if pre[0] == 1:
+            p[1] = p[1].knlm.KNLMeansCL(h=str[1])
+        elif pre[0] == 2:
+            p[1] = RemoveGrain(p[1], mode=str[1])
+    if 2 in planes:
+        if pre[0] == 1:
+            p[2] = p[2].knlm.KNLMeansCL(h=str[2])
+        elif pre[0] == 2:
+            p[2] = RemoveGrain(p[2], mode=str[2])
 
-    dns = denoise
-    dif = core.std.MakeDiff(lma, dns)
-    rgn = STPresso(dif, **kwargs)
-    mrg = core.std.MergeDiff(dns, rgn)
-    shf = core.std.ShufflePlanes([mrg, b16], [0,1,2], vs.YUV)
+    dns = join(p)
+    dif = core.std.MakeDiff(b16, dns, planes=planes)
+    rgn = STPresso(dif, **kwargs, planes=planes)
+    mrg = core.std.MergeDiff(dns, rgn, planes=planes)
 
     if show_diff is True:
         return dif
     else: 
-        return Depth(shf, clip.format.bits_per_sample)
+        return Depth(mrg, clip.format.bits_per_sample)
 
 
 def RGBtoYUV(clip):
