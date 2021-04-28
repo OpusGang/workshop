52e8cac7999491cdb8d94c274e748fca815e0a6d
NSQY <34185288+NSQY@users.noreply.github.com>
2021-04-28 16:58:39 +0000

    probably works idk

--- dysfunctional.py
+++ dysfunctional.py
@@ -1,59 +1,54 @@
 import vapoursynth as vs
 core = vs.core
 
-def STGraino(clip, planes: [0,1,2],
-            pre = [1,2,2], 
-            str: float = [1,4,4], 
-            show_diff = False,
-            **kwargs) -> vs.VideoNode:
+def STGraino(clip: vs.VideoNode, prefilter: vs.VideoNode, planes: list = [0,1,2], show_diff: bool = False, **kwargs) -> vs.VideoNode:
     """
-    KNLMeansCL documentation can be found here: https://github.com/Khanattila/KNLMeansCL/wiki/Filter-description
     STPresso: https://github.com/HomeOfVapourSynthEvolution/havsfunc/blob/master/havsfunc.py#L3535
     STPresso args can be parsed directly into this wrapper, i.e;
-    grn = dsf.STGraino(b16, planes=[0,1,2], pre=[1,2,2], str = [1.5,4,4], limit = 4, bias = 65, tbias = 50, tthr = 32, show_diff=True)
+    grn = dsf.STGraino(clip, core.bilateral.Bilateral, planes=[1,2,2], limit=4, bias=65, tbias=50, tthr=32, show_diff=True)
     -
-    TODO: parse knlm args, per-plane show_diff
+    TODO: \o/
     """
-    from vsutil import split, join, depth as Depth
+    from vsutil import split, join, depth
     from havsfunc import STPresso
-    from rgvs import RemoveGrain
 
-    rgb = RGBtoYUV(clip)
-    b16 = Depth(rgb, 16)
-    p = split(b16)
+    planar = split(clip)
 
-    if 0 in planes:
-        if pre[0] == 1:
-            p[0] = p[0].knlm.KNLMeansCL(h=str[0])
-        elif pre[0] == 2:
-            p[0] = RemoveGrain(p[0], mode=str[0])
-    if 1 in planes:
-        if pre[0] == 1:
-            p[1] = p[1].knlm.KNLMeansCL(h=str[1])
-        elif pre[0] == 2:
-            p[1] = RemoveGrain(p[1], mode=str[1])
-    if 2 in planes:
-        if pre[0] == 1:
-            p[2] = p[2].knlm.KNLMeansCL(h=str[2])
-        elif pre[0] == 2:
-            p[2] = RemoveGrain(p[2], mode=str[2])
+    if 0 in planes: planar[0] = prefilter(planar[0])
+    if 1 in planes: planar[1] = prefilter(planar[1])
+    if 2 in planes: planar[2] = prefilter(planar[2])
 
-    dns = join(p)
-    dif = core.std.MakeDiff(b16, dns, planes=planes)
+    dns = join(planar)
+
+    dif = core.std.MakeDiff(clip, dns, planes=planes)
     rgn = STPresso(dif, **kwargs, planes=planes)
     mrg = core.std.MergeDiff(dns, rgn, planes=planes)
 
+    # Living true to the name
+    # not really sure what to do about this
     if show_diff is True:
-        return dif
-    else: 
-        return Depth(mrg, clip.format.bits_per_sample)
 
+        diff = split(clip)
+        
+        diff_y = core.std.MakeDiff(planar[0], diff[0], planes=[0])
+        diff_cb = core.std.MakeDiff(planar[0], diff[0], planes=[0])
+        diff_cr = core.std.MakeDiff(planar[0], diff[0], planes=[0])
+        return core.std.ShufflePlanes([diff_y, diff_cb, diff_cr], [0,0,0], vs.YUV)
 
-def RGBtoYUV(clip):
-    if clip.format.color_family == vs.RGB:
-        return core.resize.Point(clip, format=vs.YUV444P16, matrix_s='709', dither_type='error_diffusion')
-    else:
-        return clip
+    else: return depth(mrg, clip.format.bits_per_sample)
+
+# Control grain averaging with weights
+# Higher values = weaker grain and less entropy
+def coolgrain(clip: vs.VideoNode, strength: list = [5,0], weights: list = [1] * 7, temporal_average: int = 100, luma_scaling: float = 12.0, invert: bool = False) -> vs.VideoNode:
+    if isinstance(strength, int): strength=[strength, strength]
+
+    grain = core.grain.Add(clip, var=strength[0], uvar=strength[1], seed=444)
+    average = core.std.Merge(grain, core.misc.AverageFrames(grain, weights=weights), weight=[temporal_average / 100])
+
+    mask = core.adg.Mask(core.std.PlaneStats(clip))
+    if invert is True: mask = core.std.Invert(mask)
+
+    return core.std.MaskedMerge(clip, average, mask)
 
 
 def FDOG(clip: vs.VideoNode, retinex=True, div=2, bits=16, sigma=1.5, opencl=False) -> vs.VideoNode:
