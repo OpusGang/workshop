396152da35677fffba2ace8194235d1a6af11a8e
NSQY <34185288+NSQY@users.noreply.github.com>
2023-05-10 04:51:03 +0930

    HORRIBLE CODE

--- vsdysfunctional/vsdysfunctional/metrics.py
+++ vsdysfunctional/vsdysfunctional/metrics.py
@@ -4,12 +4,14 @@ import numpy as np
 import csv
 import seaborn
 
-from vstools import vs, core, MatrixT, Matrix, clip_async_render
+from vstools import vs, core, MatrixT, Matrix, clip_async_render, merge_clip_props
+from awsmfunc import run_scenechange_detect, SceneChangeDetector
 from muvsfunc import SSIM, GMSD
 
 from enum import Enum
 from functools import partial
 from itertools import cycle
+import pandas as pd
 
 
 class Statistic(Enum):
@@ -81,7 +83,46 @@ class CSVPropThing:
 
         plt.style.use(style_file)
 
-    def write(self, clip: vs.VideoNode, async_requests: int = 1):
+    def _group_by_scene(self, prop: str | list[str] = 'psnr_y'):
+        df = pd.read_csv(self.filepath)
+
+        with open('scenechanges.txt', 'r') as f:
+            ranges = [int(line) for line in f.read().splitlines()]
+
+        scores = []
+
+        for i in range(len(ranges) - 1):
+            start = ranges[i]
+            end = ranges[i + 1] - 1
+
+            df_subset = df.iloc[start:end]
+
+            mean = df_subset[f'{prop}'].median()
+
+            scores.append(mean)
+
+        return scores
+
+    def _read(self, prop: str | list[str] = 'psnr_y'):
+        data_from_csv = []
+
+        with open(self.filepath, mode="r", newline="") as csvfile:
+            reader = csv.DictReader(csvfile)
+
+            for row in reader:
+                _prop = float(row[f'{prop}'])
+                data_from_csv.append(_prop)
+
+        return data_from_csv
+    
+    def write(self, clip: vs.VideoNode, async_requests: int = 1, scenechange: bool = True):
+        if scenechange:
+            run_scenechange_detect(
+                clip,
+                tonemap=False, output='scenechanges.txt',
+                detector=SceneChangeDetector.AvScenechange
+                )
+
         data = clip_async_render(
             clip, outfile=None, progress='Getting frame props...',
             callback=lambda _, f: f.props.copy(), async_requests=async_requests # noqa
@@ -98,23 +139,15 @@ class CSVPropThing:
             for row in data:
                 writer.writerow(row)
 
-    def _read(self, prop: str | list[str] = 'psnr_y'):
-        data_from_csv = []
-
-        with open(self.filepath, mode="r", newline="") as csvfile:
-            reader = csv.DictReader(csvfile)
-
-            for row in reader:
-                _prop = float(row[f'{prop}'])
-                data_from_csv.append(_prop)
-
-        return data_from_csv
-
-    def plot(self, prop: str | list[str] = 'psnr_y'):
+    def plot(self, prop: str | list[str] = 'psnr_y', per_scene: bool = True):
         if isinstance(prop, str):
             prop = [prop]
 
-        data = [self._read(prop=p) for p in prop]
+        if per_scene:
+            data = [self._group_by_scene(prop=p) for p in prop]
+        else:
+            data = [self._read(prop=p) for p in prop]
+
         frames = range(len(data[0]))
 
         window_size = int(0.05 * len(data[0]))
@@ -127,7 +160,7 @@ class CSVPropThing:
 
             mean = [calculate_mean(d, window_size, statistic) for statistic in Statistic]
 
-            plt.scatter(frames, d, label=p, color=palette, linewidth=0.2)
+            plt.scatter(frames, d, label=p, color=palette, linewidth=0.1)
 
             plt.plot(
                 frames, mean[3],
@@ -135,8 +168,68 @@ class CSVPropThing:
                 label=f'{p} Harmonic Mean'
                 )
 
-        plt.xlabel('frame')
+        plt.xlabel('scene' if per_scene else 'frame')
         plt.ylabel('score')
+
+        if per_scene:
+            with open('scenechanges.txt', 'r') as f:
+                numbers = [int(line) for line in f.read().splitlines()]
+
+            ranges = []
+            result = []
+
+            for i in range(len(numbers) - 1):
+
+                start = numbers[i]
+                end = numbers[i + 1]
+
+                if end >= start:
+                    ranges.append(range(start, end))
+
+            numbers = [list(r) for r in ranges]
+
+            for num_list in numbers:
+                range_str = str(num_list[0]) + "-" + str(num_list[-1])
+                result.append(range_str)
+
+            plt.xticks(ticks=[x for x in range(len(result))], labels=result)
+            plt.xticks(rotation=-45, fontsize=8)
+
+        plt.legend(loc='upper right')
+        plt.show()
+
+    def histogram(self, prop: str | list[str] = 'psnr_y'):
+        if isinstance(prop, str):
+            prop = [prop]
+
+        data = [self._read(prop=p) for p in prop]
+
+        _colours = seaborn.color_palette('pastel', as_cmap=True)
+        _palette = cycle(_colours)
+
+        for p, d in zip(prop, data):
+            plt.hist(d, 30, color=next(_palette), label=f'{p}', alpha=0.5, stacked=True)
+
+        plt.xlabel('score')
+        plt.ylabel('count')
+
+        plt.legend(loc='upper right')
+        plt.show()
+        
+    def kde(self, prop: str | list[str] = 'psnr_y'):
+        if isinstance(prop, str):
+            prop = [prop]
+
+        data = [self._read(prop=p) for p in prop]
+
+        _colours = seaborn.color_palette('pastel')
+        _palette = cycle(_colours)
+
+        for p, d in zip(prop, data):
+            seaborn.kdeplot(d, color=next(_palette), label=f'{p}')
+
+        plt.xlabel('score')
+        plt.ylabel('count')
 
         plt.legend(loc='upper right')
         plt.show()
@@ -168,38 +261,44 @@ def metrics(
 
     if metric in (Metric.PSNR, Metric.PSNR_HVS, Metric.SSIM, Metric.SSIM_MS, Metric.GMSD):
         if reference.format.color_family != vs.YUV:
-            reference, distorted = [core.resize.Spline64(
+            _reference, _distorted = [core.resize.Spline64(
                 i, format=vs.YUV420P12, matrix=matrix, dither_type='error_diffusion'
                 ) for i in (reference, distorted)]
+        else:
+            _reference, _distorted = reference, distorted
 
         if metric == Metric.SSIM:
-            ssim = [SSIM(clip1=reference, clip2=distorted, plane=i) for i in range(3)]
+            ssim = [SSIM(clip1=_reference, clip2=_distorted, plane=i) for i in range(3)]
             measure = core.std.FrameEval(
-                reference, prop_src=ssim,
-                eval=partial(_PlaneSSIMTransfer, clip=reference, src_prop='PlaneSSIM', out_prop='SSIM'))
+                _reference, prop_src=ssim,
+                eval=partial(_PlaneSSIMTransfer, clip=reference, src_prop='PlaneSSIM', out_prop='SSIM')
+                )
+
         elif metric == Metric.GMSD:
-            gmsd = [GMSD(clip1=reference, clip2=distorted, plane=i) for i in range(3)]
+            gmsd = [GMSD(clip1=_reference, clip2=_distorted, plane=i) for i in range(3)]
             measure = core.std.FrameEval(
-                reference, prop_src=gmsd,
-                eval=partial(_PlaneSSIMTransfer, clip=reference, src_prop='PlaneGMSD', out_prop='GMSD')
+                _reference, prop_src=gmsd,
+                eval=partial(_PlaneSSIMTransfer, clip=_reference, src_prop='PlaneGMSD', out_prop='GMSD')
                 )
         else:
-            measure = core.vmaf.Metric(reference=reference, distorted=distorted, feature=metric.value)
+            measure = core.vmaf.Metric(reference=_reference, distorted=_distorted, feature=metric.value)
 
     elif metric in (Metric.SSIMULACRA1, Metric.SSIMULACRA2, Metric.BUTTER):
         if reference.format.color_family != vs.RGB:
-            reference, distorted = [core.resize.Spline64(
-                i, format=vs.RGB24, matrix=matrix, dither_type='error_diffusion'
+            _reference, _distorted = [core.resize.Spline64(
+                i, format=vs.RGB24, matrix_in=matrix, dither_type='error_diffusion'
                 ) for i in (reference, distorted)]
- 
+        else:
+            _reference, _distorted = reference, distorted
+
         if metric == Metric.SSIMULACRA1:
-            measure = core.julek.SSIMULACRA(reference=reference, distorted=distorted, feature=1, **args)
+            measure = core.julek.SSIMULACRA(reference=_reference, distorted=_distorted, feature=1, **args)
         elif metric == Metric.SSIMULACRA2:
-            measure = core.julek.SSIMULACRA(reference=reference, distorted=distorted, feature=0, **args)
+            measure = core.julek.SSIMULACRA(reference=_reference, distorted=_distorted, feature=0, **args)
         elif metric == Metric.BUTTER:
-            measure = core.julek.Butteraugli(reference=reference, distorted=distorted, **args)
+            measure = core.julek.Butteraugli(reference=_reference, distorted=_distorted, **args)
  
     else:
         measure = core.std.PlaneStats(reference, distorted, **args)
  
-    return measure
+    return merge_clip_props(reference, measure)
